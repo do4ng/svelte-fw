4ef73ad2ebcbc4f8a30c3f12a6a5fcadbe6f8767
r.ehdals <57896501+kangdongmandoo@users.noreply.github.com>
2022-01-22 12:46:09 +0900

    Delete framework/dist directory

--- framework/dist/utils/time.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function GetTime(date = new Date()) {
-    return `[${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}]`;
-}
-exports.default = GetTime;
