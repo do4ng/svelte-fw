be41cabaa82a43252c338cb71397d29f5bee6805
r.ehdals <57896501+kangdongmandoo@users.noreply.github.com>
2022-01-23 12:54:25 +0900

    add `alias`

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import sveltePreprocess from 'svelte-preprocess';
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   root: './',
   publicDir: '/public/',
-  plugins: [svelte()],
+  plugins: [svelte({ preprocess: sveltePreprocess() })],
   resolve: {
     alias: {
-      '@components': path.resolve(__dirname, './components'),
-      '@lib': path.resolve(__dirname, './lib'),
+      $components: path.resolve(__dirname, './components'),
+      $lib: path.resolve(__dirname, './lib'),
     },
   },
 });
